bfc59c7f47be7aa371c8538c627cbd46c6cba9ad
OstapMykhailiv <115294426+OstapMykhailiv@users.noreply.github.com>
2023-10-05 20:07:41 +0300

    Add files via upload

--- lr1/3/main.js
+++ lr1/3/main.js
@@ -1,42 +1,13 @@
-var number = prompt("Введіть цифру місяця року");
+var number = prompt("Введіть цифру місяця: ");
 
-switch (parseInt(number)) {
-  case 1:
-    alert("Січень");
-    break;
-  case 2:
-    alert("Лютий");
-    break;
-  case 3:
-    alert("Березень");
-    break;
-  case 4:
-    alert("Квітень");
-    break;
-  case 5:
-    alert("Травень");
-    break;
-  case 6:
-    alert("Червень");
-    break;
-  case 7:
-    alert("Липень");
-    break;
-  case 8:
-    alert("Серпень");
-    break;
-  case 9:
-    alert("Вересень");
-    break;
-  case 10:
-    alert("Жовтень");
-    break;
-  case 11:
-    alert("Листопад");
-    break;
-  case 12:
-    alert("Грудень");
-    break;
-  default:
-    alert("Такого місяця не існує/ви ввели некоректні дані");
+if (number >= 1 && number <= 2 || number === 12) {
+  alert("Зима");
+} else if (number >= 3 && number <= 5) {
+  alert("Весна");
+} else if (number >= 6 && number <= 8) {
+  alert("Літо");
+} else if (number >= 9 && number <= 11) {
+  alert("Осінь");
+} else {
+  alert("Неравильне число місяця. Введіть число від 1 до 12.");
 }
